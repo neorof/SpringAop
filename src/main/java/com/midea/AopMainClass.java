17de3c4221ccf1cd83cf39c6919210516f06b196
Changxiong zhou <693946083@qq.com>
2018-07-02 19:41:29 +0800

    test

--- src/main/java/com/midea/AopMainClass.java
+++ src/main/java/com/midea/AopMainClass.java
@@ -8,7 +8,7 @@ public class AopMainClass {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "classpath:applicationContext.xml" });
+                new String[] { "classpath:applicationContext.xmll" });
         context.start();
         MyApoClass apoClass = context.getBean(MyApoClass.class);
         apoClass.method();
